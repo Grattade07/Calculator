1aa3ece7d8d8342f6c5bcfd28521bc5be7780f8a
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-09-17 16:56:41 +0100

    Add, subtract, multiply + divide functions made

---
+++ script.js
@@ -0,0 +1,15 @@
+function addition(a, b) {
+    console.log(a + b);
+}
+
+function subtraction(a, b) {
+    console.log(a - b);
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+function divide(a, b) {
+    console.log (a / b);
+}
