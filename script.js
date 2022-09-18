fd4c9b6cd7fdb5fc369f3aa8a5255a74da8ec20d
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-09-18 17:20:51 +0100

    Display value replaced by numBtn value

--- script.js
+++ script.js
@@ -30,20 +30,16 @@ const display = document.querySelector('#display');
 
 let displayValue = 0;
 
-display.append(displayValue);
+display.textContent = displayValue ;
 
 const padNum = document.querySelectorAll(".num");
 
 //Edited method to remove current value on display and replace with new displayValue
 
 padNum.forEach((button) => {
-    button.addEventListener("click", () => {
-    if (displayValue = 0) {
-        display.textContent = " ";
-        displayValue += Number(button.textContent);
-    }
-    console.log(displayValue += Number(button.textContent));
-    display.append(displayValue);
-    })
+    button.addEventListener("click", () => {  
+    displayValue = button.textContent;
+    return display.textContent = displayValue;
 })
+});
 
